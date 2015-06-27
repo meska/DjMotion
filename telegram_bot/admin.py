741a96dcb5493e1119898560cce7117a2dfc3e1a
Meska <meskatech@gmail.com>
2015-06-27 15:37:34 +0200

    submodules ok

--- telegram_bot/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from telegram_bot.models import *
-# Register your models here.
-
-class TelegramUserAdmin(admin.ModelAdmin):
-    list_display = ('user_id','username', 'first_name', 'last_name')
-    #list_filter = ('server__name',)
-    
-admin.site.register(TelegramUser,TelegramUserAdmin)
--- telegram_bot/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls import include, url
-
-urlpatterns = [
-    url(r'^webhook', 'telegram_bot.views.webhook'),
-]
